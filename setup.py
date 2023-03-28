737dc84b26a65e5afb5c79fed518011bdf540a19
jarathomas <jaratthomas@gmail.com>
2023-03-28 17:08:21 -0400

    bug fix in setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,9 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 about = {}
 with open(
-        os.path.join(here, "pyopenva", "__version__.py"), "r", "utf-8") as f:
+        os.path.join(here, "pyopenva", "__version__.py"),
+        mode="r",
+        encoding="utf-8") as f:
     exec(f.read(), about)
 
 try:
